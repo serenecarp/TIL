4459640b6912f8a428bf4d46fd0f2e884fb22c2a
serenecarp <spmjoon516@gmail.com>
2023-01-25 16:06:15 +0900

    0125 updated

---
+++ day0125/src/test01/Dog.java
@@ -0,0 +1,11 @@
+package test01;
+
+public class Dog extends Animal{
+
+	@Override
+	public void speak() {
+		// TODO Auto-generated method stub
+		System.out.println("멍멍");
+	}
+	
+}
---
+++ day0125/src/test04/Cat.java
@@ -0,0 +1,19 @@
+package test04;
+
+
+// 일반클래스인데 추상클래스를 상속받는다.
+public class Cat extends Animal{
+	// 자식클래스는 부모 클래스의 추상메소드를 모두 오버라이드해야한다
+	
+	@Override
+	public void speak() {
+		// TODO Auto-generated method stub
+		System.out.println("야옹");
+	}
+
+	
+	// 자식클래스에서 독단적으로 eat()메소드를 만들어버렸다
+	public void eat() {
+		System.out.println("생선을 먹어요.");
+	}
+}
